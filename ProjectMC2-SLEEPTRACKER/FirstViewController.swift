8b11f4d2db8f77e5e35a00572b1c0ac7954e9bd6
Stendy Antonio <stendy20.sa@gmail.com>
2020-05-05 12:17:17 +0700

    Initial Commit

---
+++ ProjectMC2-SLEEPTRACKER/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ProjectMC2-SLEEPTRACKER
+//
+//  Created by Stendy Antonio on 05/05/20.
+//  Copyright © 2020 Stendy Antonio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectMC2-SLEEPTRACKER/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ProjectMC2-SLEEPTRACKER
+//
+//  Created by Stendy Antonio on 05/05/20.
+//  Copyright © 2020 Stendy Antonio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
