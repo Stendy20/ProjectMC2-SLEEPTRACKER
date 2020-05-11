9fac41ae128056076cd83fc08bb02cdce58dc289
Stendy Antonio <stendy20.sa@gmail.com>
2020-05-11 10:34:21 +0700

    Stendy Commit

---
+++ ProjectMC2-SLEEPTRACKER/DetailStoryController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailStoryController.swift
+//  ProjectMC2-SLEEPTRACKER
+//
+//  Created by Stendy Antonio on 11/05/20.
+//  Copyright © 2020 Stendy Antonio. All rights reserved.
+//
+
+import UIKit
+
+class DetailStoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
