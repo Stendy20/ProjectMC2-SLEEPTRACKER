ce45d28a6a41978bdacd48a79982d16907a4b017
Stendy Antonio <stendy20.sa@gmail.com>
2020-05-19 12:03:31 +0700

    Stendy Commit

---
+++ ProjectMC2-SLEEPTRACKER/LeftEditTimeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LeftEditTimeViewController.swift
+//  ProjectMC2-SLEEPTRACKER
+//
+//  Created by Stendy Antonio on 19/05/20.
+//  Copyright © 2020 Stendy Antonio. All rights reserved.
+//
+
+import UIKit
+
+class LeftEditTimeViewController: UIViewController {
+    
+    @IBOutlet weak var TimePickerEdit: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handledone))
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func handledone(){
+        print("Button Done")
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
--- ProjectMC2-SLEEPTRACKER/SecondViewController.swift
+++ ProjectMC2-SLEEPTRACKER/SecondViewController.swift
@@ -15,10 +15,10 @@ class SecondViewController: UIViewController {
     
     var timer: Timer?
     
-    var hour = 0
-    
-    var hourcurrent = 0
-    var timecurrent1 = ""
+//    var hour = 0
+//
+//    var hourcurrent = 0
+//    var timecurrent1 = ""
     var timevalue = ""
     
     override func viewDidLoad() {
@@ -29,49 +29,49 @@ class SecondViewController: UIViewController {
     @IBAction func SleepTimePickerAction(_ sender: Any) {
         
         let dateformatter = DateFormatter()
-        let dateformatter1 = DateFormatter()
+//        let dateformatter1 = DateFormatter()
         
         dateformatter.dateFormat = "HH : mm"
-        dateformatter1.dateFormat = "HH"
+//        dateformatter1.dateFormat = "HH"
         
         timevalue = dateformatter.string(from: SleepTimePicker.date)
         
-        hour = Int(dateformatter1.string(from: SleepTimePicker.date))!
+//        hour = Int(dateformatter1.string(from: SleepTimePicker.date))!
         
-        getCurrentTime()
-        
-    }
-    
-    @objc func getCurrentTime(){
-        let timeformatter = DateFormatter()
-        let timeformatter1 = DateFormatter()
-        
-        timeformatter.dateFormat = "HH"
-        timeformatter1.dateFormat = "HH : mm"
-        
-        hourcurrent = Int(timeformatter.string(from: Date()))!
-        
-        timecurrent1 = timeformatter1.string(from: Date())
-    }
-    
-    @IBAction func StartSleepButton(_ sender: Any) {
-          timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(CompareTime), userInfo: nil, repeats: true)
-      }
-    
-    @objc func CompareTime(){
-        if(timecurrent1 == timevalue){
-            timer?.invalidate()
-
-            let alert = UIAlertController(title: "Reminder", message: "WAKEUP", preferredStyle: .alert)
-            let action = UIAlertAction(title: "Ok", style: .cancel)
-            alert.addAction(action)
-            self.present(alert, animated: true, completion: nil)
-            
-        }
-        getCurrentTime()
+//        getCurrentTime()
         
     }
     
+//    @objc func getCurrentTime(){
+//        let timeformatter = DateFormatter()
+//        let timeformatter1 = DateFormatter()
+//
+//        timeformatter.dateFormat = "HH"
+//        timeformatter1.dateFormat = "HH : mm"
+//
+//        hourcurrent = Int(timeformatter.string(from: Date()))!
+//
+//        timecurrent1 = timeformatter1.string(from: Date())
+//    }
+    
+//    @IBAction func StartSleepButton(_ sender: Any) {
+//          timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(CompareTime), userInfo: nil, repeats: true)
+//      }
+    
+//    @objc func CompareTime(){
+//        if(timecurrent1 == timevalue){
+//            timer?.invalidate()
+//
+//            let alert = UIAlertController(title: "Reminder", message: "WAKEUP", preferredStyle: .alert)
+//            let action = UIAlertAction(title: "Ok", style: .cancel)
+//            alert.addAction(action)
+//            self.present(alert, animated: true, completion: nil)
+//
+//        }
+//        getCurrentTime()
+//
+//    }
+//
     
   
     
