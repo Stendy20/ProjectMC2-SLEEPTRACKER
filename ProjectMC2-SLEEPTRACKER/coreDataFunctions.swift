4d483519f95a766bd41d0fc974b49960f1a1bc45
Michael Sanjaya <>
2020-05-20 19:30:03 +0700

    Defined entities for Core Data, added file for core data functions

---
+++ ProjectMC2-SLEEPTRACKER/coreDataFunctions.swift
@@ -0,0 +1,11 @@
+//
+//  coreDataFunctions.swift
+//  ProjectMC2-SLEEPTRACKER
+//
+//  Created by Michael Sanjaya on 20/05/20.
+//  Copyright © 2020 Stendy Antonio. All rights reserved.
+//
+
+import Foundation
+
+
