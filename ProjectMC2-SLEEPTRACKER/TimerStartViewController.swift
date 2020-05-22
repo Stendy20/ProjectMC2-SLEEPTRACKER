9ab50237075e7a6c4dd8a3704a044c1feb6a2149
Stendy Antonio <stendy20.sa@gmail.com>
2020-05-22 16:44:34 +0700

    Stendy Commit Alarm Page

--- ProjectMC2-SLEEPTRACKER/TimerStartViewController.swift
+++ ProjectMC2-SLEEPTRACKER/TimerStartViewController.swift
@@ -12,11 +12,27 @@ class TimerStartViewController: UIViewController {
     
     @IBOutlet weak var LongPressButton: UIButton!
     
+    @IBOutlet weak var RedButtonBackground: UIButton!
+    @IBOutlet weak var GreenButtonBackground: UIButton!
+    @IBOutlet weak var BlueButtonBackground: UIButton!
+    
+    @IBOutlet weak var BackgroundImage: UIImageView!
+    
+    @IBOutlet weak var LabelCurrentITIME: UILabel!
+    @IBOutlet weak var LabelSetTime: UILabel!
+    
     @IBOutlet weak var LabelTime: UILabel!
+    
+    @IBOutlet weak var PlayMusicButton: UIButton!
+    @IBOutlet weak var RepeatMusicButton: UIButton!
+    @IBOutlet weak var MuteMusicButton: UIButton!
     
     var flag = 0
     
     var timer: Timer?
+    var timertest: Timer?
+    
+    var timecurrent = ""
     
     var count = 5
     
@@ -25,15 +41,20 @@ class TimerStartViewController: UIViewController {
         
         navigationItem.hidesBackButton = true
         
+        LabelSetTime.text = "\(retrieveTime().wakeUpTime) AM"
+        
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(yourAction))
-//        longPress.minimumPressDuration = 5
+        //        longPress.minimumPressDuration = 5
         LongPressButton.addGestureRecognizer(longPress)
+        
+        timertest = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(getCurrentTime), userInfo: nil, repeats: true)
+
     }
     
     @objc func yourAction(Recognizer: UILongPressGestureRecognizer){
         if Recognizer.state == .began{
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
-
+            
         }
         else if Recognizer.state == .ended{
             if flag == 0{
@@ -45,7 +66,7 @@ class TimerStartViewController: UIViewController {
                 flag = 0
             }
         }
-     
+        
     }
     
     @objc func fireTimer(){
@@ -68,6 +89,36 @@ class TimerStartViewController: UIViewController {
         }
         
     }
+    
+    @objc func getCurrentTime(){
+        let timeformatter1 = DateFormatter()
+        
+        timeformatter1.dateFormat = "HH : mm"
+        
+        timecurrent = timeformatter1.string(from: Date())
+        
+        LabelCurrentITIME.text = timecurrent
+    }
+    
+    
+    @IBAction func RedButtonAction(_ sender: Any) {
+        BackgroundImage.image = #imageLiteral(resourceName: "rain_bg_2")
+    }
+    @IBAction func GreenButtonAction(_ sender: Any) {
+        BackgroundImage.image = #imageLiteral(resourceName: "forest_bg_1")
+    }
+    @IBAction func BlueButtonAction(_ sender: Any) {
+        BackgroundImage.image = #imageLiteral(resourceName: "ocean_bg")
+    }
+    
+    
+    @IBAction func MusicPlayAction(_ sender: Any) {
+    }
+    @IBAction func MusicRepeatAction(_ sender: Any) {
+    }
+    @IBAction func MusicMuteAction(_ sender: Any) {
+    }
+    
     
     /*
      // MARK: - Navigation
