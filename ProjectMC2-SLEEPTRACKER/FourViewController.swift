13947dcee25cffaf663e8f1af8d320f887d33597
Stendy Antonio <stendy20.sa@gmail.com>
2020-05-26 09:45:39 +0700

    Stendy Commit

--- ProjectMC2-SLEEPTRACKER/FourViewController.swift
+++ ProjectMC2-SLEEPTRACKER/FourViewController.swift
@@ -17,6 +17,16 @@ class FourViewController: UIViewController {
     //        var wakeUpTime: String
     //    }
     
+    var monday = 0
+    var tuesday = 0
+    var wednesday = 0
+    var thursday = 0
+    var friday = 0
+    var saturday = 0
+    var sunday = 0
+    
+    var flag = 0
+    
     @IBOutlet weak var BedTimeLabel: UILabel!
     @IBOutlet weak var WakeUpLabel: UILabel!
     
@@ -54,27 +64,130 @@ class FourViewController: UIViewController {
     func checkday(){
         if retrieveDay().mon == 1 {
             MondayButton.setImage(#imageLiteral(resourceName: "mon_on"), for: .normal)
+            monday = 1
         }
         if retrieveDay().tue == 1 {
             TuesdayButton.setImage(#imageLiteral(resourceName: "tue_on"), for: .normal)
+            tuesday = 1
         }
         if retrieveDay().wed == 1 {
             WednesdayButton.setImage(#imageLiteral(resourceName: "wed_on"), for: .normal)
+            wednesday = 1
         }
         if retrieveDay().thu == 1 {
             ThurdayButton.setImage(#imageLiteral(resourceName: "tue_on"), for: .normal)
+            thursday = 1
         }
         if retrieveDay().fri == 1 {
             FridayBUtton.setImage(#imageLiteral(resourceName: "fri_on"), for: .normal)
+            friday = 1
         }
         if retrieveDay().sat == 1 {
             SaturdayButton.setImage(#imageLiteral(resourceName: "sun_on"), for: .normal)
+            saturday = 1
         }
         if retrieveDay().sun == 1 {
             SundayButton.setImage(#imageLiteral(resourceName: "sun_on"), for: .normal)
+            sunday = 1
         }
+        
+        
     }
     
+    @IBAction func MondayButton(_ sender: Any) {
+        if flag == 0{
+            flag = 1
+            MondayButton.setImage(#imageLiteral(resourceName: "mon_on"), for: .normal)
+            monday = 1
+        }
+        else if flag == 1 {
+            flag = 0
+            MondayButton.setImage(#imageLiteral(resourceName: "mon_off"), for: .normal)
+            monday = 0
+        }
+        storeDay(mon: monday, tue: tuesday, wed: wednesday, thu: thursday, fri: friday, sat: saturday, sun: sunday)
+        
+    }
+    @IBAction func TuesdayButton(_ sender: Any) {
+        if flag == 0{
+            TuesdayButton.setImage(#imageLiteral(resourceName: "tue_on"), for: .normal)
+            flag = 1
+            tuesday = 1
+        }
+        else if flag == 1 {
+            TuesdayButton.setImage(#imageLiteral(resourceName: "tue_off"), for: .normal)
+            flag = 0
+            tuesday = 0
+        }
+        storeDay(mon: monday, tue: tuesday, wed: wednesday, thu: thursday, fri: friday, sat: saturday, sun: sunday)
+        
+    }
+    @IBAction func WednesdayButton(_ sender: Any) {
+        if flag == 0{
+            WednesdayButton.setImage(#imageLiteral(resourceName: "wed_on"), for: .normal)
+            flag = 1
+            wednesday = 1
+        }
+        else if flag == 1 {
+            WednesdayButton.setImage(#imageLiteral(resourceName: "wed_off"), for: .normal)
+            flag = 0
+            wednesday = 0
+        }
+        storeDay(mon: monday, tue: tuesday, wed: wednesday, thu: thursday, fri: friday, sat: saturday, sun: sunday)
+        
+    }
+    @IBAction func ThursdayButton(_ sender: Any) {
+        if flag == 0{
+            ThurdayButton.setImage(#imageLiteral(resourceName: "thu_on"), for: .normal)
+            flag = 1
+            thursday = 1
+        }
+        else if flag == 1 {
+            ThurdayButton.setImage(#imageLiteral(resourceName: "tue_off"), for: .normal)
+            flag = 0
+            thursday = 0
+        }
+        storeDay(mon: monday, tue: tuesday, wed: wednesday, thu: thursday, fri: friday, sat: saturday, sun: sunday)
+    }
+    @IBAction func FridayButton(_ sender: Any) {
+        if flag == 0{
+            FridayBUtton.setImage(#imageLiteral(resourceName: "fri_on"), for: .normal)
+            flag = 1
+            friday = 1
+        }
+        else if flag == 1 {
+            FridayBUtton.setImage(#imageLiteral(resourceName: "fri_off"), for: .normal)
+            flag = 0
+            friday = 0
+        }
+        storeDay(mon: monday, tue: tuesday, wed: wednesday, thu: thursday, fri: friday, sat: saturday, sun: sunday)
+    }
+    @IBAction func SaturdayButton(_ sender: Any) {
+        if flag == 0{
+            SaturdayButton.setImage(#imageLiteral(resourceName: "sat_on"), for: .normal)
+            flag = 1
+            saturday = 1
+        }
+        else if flag == 1 {
+            SaturdayButton.setImage(#imageLiteral(resourceName: "sun_off"), for: .normal)
+            flag = 0
+            saturday = 0
+        }
+        storeDay(mon: monday, tue: tuesday, wed: wednesday, thu: thursday, fri: friday, sat: saturday, sun: sunday)
+    }
+    @IBAction func SundayButton(_ sender: Any) {
+        if flag == 0{
+            SundayButton.setImage(#imageLiteral(resourceName: "sun_on"), for: .normal)
+            flag = 1
+            sunday = 1
+        }
+        else if flag == 1 {
+            SundayButton.setImage(#imageLiteral(resourceName: "sun_off"), for: .normal)
+            flag = 0
+            sunday = 0
+        }
+        storeDay(mon: monday, tue: tuesday, wed: wednesday, thu: thursday, fri: friday, sat: saturday, sun: sunday)
+    }
     
     /*
      // MARK: - Navigation
