f172ccb8fde6f2c3d27b75d1104ea912a456112a
Stendy Antonio <stendy20.sa@gmail.com>
2020-05-27 09:55:34 +0700

    Stendy Commit

--- ProjectMC2-SLEEPTRACKER/OnBoardingNavigationViewController.swift
+++ ProjectMC2-SLEEPTRACKER/OnBoardingNavigationViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class OnBoardingNavigationViewController: UINavigationController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         checkOnboarding()
@@ -17,15 +17,24 @@ class OnBoardingNavigationViewController: UINavigationController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
-    */
-
+    
+    override open var shouldAutorotate: Bool {
+        return false
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
